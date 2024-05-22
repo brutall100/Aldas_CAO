1fbae5c3890dc0e7052c2918b728f45be84e333c
Aldas <brutall100@gmail.com>
2024-05-22 20:45:18 +0300

    Third folder added

---
+++ 2024-05-09_First_My_Page/some.js
@@ -0,0 +1,11 @@
+document.getElementById("index-btn").addEventListener("click", function(){
+    let hidenUl = document.getElementsByClassName("index-hiden-ul")[0]
+    if (hidenUl.style.display === "none"){
+        hidenUl.style.display = "block"
+    } else {
+        hidenUl.style.display ="none"
+    }
+})
+
+
+// Rasyta su gpt pagalba. Not by self
