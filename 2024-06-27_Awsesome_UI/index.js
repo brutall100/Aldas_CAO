32d959ac463ce00a9664e2b9f16ba8c7399f353e
Aldas <brutall100@gmail.com>
2024-06-29 18:10:27 +0300

    Added js

--- 2024-06-27_Awsesome_UI/index.js
+++ 2024-06-27_Awsesome_UI/index.js
@@ -7,3 +7,7 @@ function changeImageD_s(card, newImageUrl) {
   var img = card.querySelector("img");
   img.src = newImageUrl;
 }
+function changeImageF(card, newImageUrl) {
+  var img = card.querySelector("img");
+  img.src = newImageUrl;
+}
