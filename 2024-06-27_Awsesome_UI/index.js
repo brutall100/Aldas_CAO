9bd915a8652b621ce7c7ccefaafc8dd7c0c9eb00
Aldas <brutall100@gmail.com>
2024-06-29 15:52:03 +0300

    D section done

--- 2024-06-27_Awsesome_UI/index.js
+++ 2024-06-27_Awsesome_UI/index.js
@@ -2,3 +2,8 @@ function changeImage(card, newImageUrl) {
   var img = card.querySelector("img");
   img.src = newImageUrl;
 }
+
+function changeImageD_s(card, newImageUrl) {
+  var img = card.querySelector("img");
+  img.src = newImageUrl;
+}
