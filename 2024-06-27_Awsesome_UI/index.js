423f03a58b8d2778aa94dcc946618025c11deccf
Aldas <brutall100@gmail.com>
2024-06-30 20:00:35 +0300

    Some css for mobile

--- 2024-06-27_Awsesome_UI/index.js
+++ 2024-06-27_Awsesome_UI/index.js
@@ -7,7 +7,19 @@ function changeImageD_s(card, newImageUrl) {
   var img = card.querySelector("img");
   img.src = newImageUrl;
 }
+
 function changeImageF(card, newImageUrl) {
   var img = card.querySelector("img");
   img.src = newImageUrl;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
+});
+
+// CHAT GPT 
