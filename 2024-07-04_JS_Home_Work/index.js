2b720f99001b801b77d4527d4dd23d1c55a5328e
Aldas <brutall100@gmail.com>
2024-07-08 20:32:38 +0300

    Mini update with Math

--- 2024-07-04_JS_Home_Work/index.js
+++ 2024-07-04_JS_Home_Work/index.js
@@ -18,10 +18,11 @@ if (password.length <= 16) {
 }
 
 // AMŽIUS
-const schoolHummanAge = prompt('Įveskite vaiko amžių:')
-const numericAge = Number(schoolHummanAge)
+const schoolHumanAge = prompt('Įveskite vaiko amžių:');
+const numericAge = Math.floor(Number(schoolHumanAge));
 
-console.log(numericAge)
+console.log(numericAge);
+
 
 if (numericAge <= 0 || numericAge > 120) {
    console.log('Klaida. Netinkamas amžius.')
@@ -48,108 +49,108 @@ if (numericAge <= 0 || numericAge > 120) {
 }
 
 // GALVOSŪKIS
-const gameFirstLvl = 1
-const gameSecondLvl = 2
-const gameThirdLvl = 3
+// const gameFirstLvl = 1
+// const gameSecondLvl = 2
+// const gameThirdLvl = 3
 
-let userAnswer1, userAnswer2, userAnswer3
+// let userAnswer1, userAnswer2, userAnswer3
 
 // //// 1 lygis
-if (gameFirstLvl == 1) {
-   userAnswer1 = +prompt('Kiek yra 1 + 1?')
-   console.log(userAnswer1)
+// if (gameFirstLvl == 1) {
+//    userAnswer1 = +prompt('Kiek yra 1 + 1?')
+//    console.log(userAnswer1)
 
-   userAnswer2 = +prompt('Kiek yra 1 + 2?')
-   console.log(userAnswer2)
+//    userAnswer2 = +prompt('Kiek yra 1 + 2?')
+//    console.log(userAnswer2)
 
-   if (userAnswer1 === 2 && userAnswer2 === 3) {
-      console.log('Teisingai! Atsakei į abu klausimus!')
-      console.log('Patekai i kita lygi!')
-   } else if (userAnswer1 === 2 && userAnswer2 !== 3) {
-      console.log('Patekai i kita lygi: pirmas atsakymas buvo teisingas, o antras buvo neteisingas.')
-   } else if (userAnswer1 !== 2 && userAnswer2 === 3) {
-      console.log('Patekai i kita lygi: antras atsakymas buvo teisingas, o pirmas buvo neteisingas.')
-   } else {
-      console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
-   }
-} else {
-   console.log('Klaida. Nepavyko patekti i kita lygi.')
-}
+//    if (userAnswer1 === 2 && userAnswer2 === 3) {
+//       console.log('Teisingai! Atsakei į abu klausimus!')
+//       console.log('Patekai i kita lygi!')
+//    } else if (userAnswer1 === 2 && userAnswer2 !== 3) {
+//       console.log('Patekai i kita lygi: pirmas atsakymas buvo teisingas, o antras buvo neteisingas.')
+//    } else if (userAnswer1 !== 2 && userAnswer2 === 3) {
+//       console.log('Patekai i kita lygi: antras atsakymas buvo teisingas, o pirmas buvo neteisingas.')
+//    } else {
+//       console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
+//    }
+// } else {
+//    console.log('Klaida. Nepavyko patekti i kita lygi.')
+// }
 
 // //// 2 lygis
-if (gameSecondLvl == 2) {
-   userAnswer2 = +prompt('Kiek yra 2 + 2?')
-   console.log(userAnswer2)
+// if (gameSecondLvl == 2) {
+//    userAnswer2 = +prompt('Kiek yra 2 + 2?')
+//    console.log(userAnswer2)
 
-   userAnswer3 = +prompt('Kiek yra 2 + 3?')
-   console.log(userAnswer3)
+//    userAnswer3 = +prompt('Kiek yra 2 + 3?')
+//    console.log(userAnswer3)
 
-   if (userAnswer2 === 4 && userAnswer3 === 5) {
-      console.log('Teisingai! Atsakei į abu klausimus!')
-      console.log('Patekai i kita lygi!')
-   } else if (userAnswer2 !== 4 && userAnswer3 === 5) {
-      console.log('Nepatekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.')
-   } else if (userAnswer2 === 4 && userAnswer3 !== 5) {
-      console.log('Nepatekai i kita lygi: pirmas atsakymas buvo teisingas, o antras buvo neteisingas.')
-   } else {
-      console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
-   }
-} else {
-   console.log('Klaida. Nepavyko patekti i kita lygi.')
-}
+//    if (userAnswer2 === 4 && userAnswer3 === 5) {
+//       console.log('Teisingai! Atsakei į abu klausimus!')
+//       console.log('Patekai i kita lygi!')
+//    } else if (userAnswer2 !== 4 && userAnswer3 === 5) {
+//       console.log('Nepatekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.')
+//    } else if (userAnswer2 === 4 && userAnswer3 !== 5) {
+//       console.log('Nepatekai i kita lygi: pirmas atsakymas buvo teisingas, o antras buvo neteisingas.')
+//    } else {
+//       console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
+//    }
+// } else {
+//    console.log('Klaida. Nepavyko patekti i kita lygi.')
+// }
 
 // //// 3 lygis
-if (gameThirdLvl == 3) {
-   userAnswer1 = +prompt('Kiek yra 3 + 3?')
-   console.log(userAnswer1)
+// if (gameThirdLvl == 3) {
+//    userAnswer1 = +prompt('Kiek yra 3 + 3?')
+//    console.log(userAnswer1)
 
-   userAnswer2 = +prompt('Kiek yra 3 + 4?')
-   console.log(userAnswer2)
+//    userAnswer2 = +prompt('Kiek yra 3 + 4?')
+//    console.log(userAnswer2)
 
-   userAnswer3 = +prompt('Kiek yra 3 + 5?')
-   console.log(userAnswer3)
+//    userAnswer3 = +prompt('Kiek yra 3 + 5?')
+//    console.log(userAnswer3)
 
-   if (userAnswer1 === 6 && userAnswer2 === 7 && userAnswer3 === 8) {
-      console.log('Teisingai! Atsakei į visus klausimus!')
-      console.log('Laimejote!')
-   } else if (userAnswer1 === 6 && userAnswer2 === 7 && userAnswer3 !== 8) {
-      console.log('Patekai i kita lygi: pirmas ir antras atsakymai buvo teisingi, o trecias buvo neteisingas.')
-   } else if (userAnswer1 === 6 && userAnswer2 !== 7 && userAnswer3 === 8) {
-      console.log('Patekai i kita lygi: pirmas ir trecias atsakymai buvo teisingi, o antras buvo neteisingas.')
-   } else if (userAnswer1 !== 6 && userAnswer2 === 7 && userAnswer3 === 8) {
-      console.log('Patekai i kita lygi: antras ir trecias atsakymai buvo teisingi, o pirmas buvo neteisingas.')
-   } else if (userAnswer1 === 6 && userAnswer2 !== 7 && userAnswer3 !== 8) {
-      console.log('Nepatekai i kita lygi: pirmas atsakymas buvo teisingas, o kiti buvo neteisingi.')
-   } else if (userAnswer1 !== 6 && userAnswer2 === 7 && userAnswer3 !== 8) {
-      console.log('Nepatekai i kita lygi: antras atsakymas buvo teisingas, o kiti buvo neteisingi.')
-   } else if (userAnswer1 !== 6 && userAnswer2 !== 7 && userAnswer3 === 8) {
-      console.log('Nepatekai i kita lygi: trecias atsakymas buvo teisingas, o kiti buvo neteisingi.')
-   } else {
-      console.log('Nepatekai i kita lygi: visi atsakymai buvo neteisingi.')
-   }
-} else {
-   console.log('Klaida. Nepavyko patekti i kita lygi.')
-}
+//    if (userAnswer1 === 6 && userAnswer2 === 7 && userAnswer3 === 8) {
+//       console.log('Teisingai! Atsakei į visus klausimus!')
+//       console.log('Laimejote!')
+//    } else if (userAnswer1 === 6 && userAnswer2 === 7 && userAnswer3 !== 8) {
+//       console.log('Patekai i kita lygi: pirmas ir antras atsakymai buvo teisingi, o trecias buvo neteisingas.')
+//    } else if (userAnswer1 === 6 && userAnswer2 !== 7 && userAnswer3 === 8) {
+//       console.log('Patekai i kita lygi: pirmas ir trecias atsakymai buvo teisingi, o antras buvo neteisingas.')
+//    } else if (userAnswer1 !== 6 && userAnswer2 === 7 && userAnswer3 === 8) {
+//       console.log('Patekai i kita lygi: antras ir trecias atsakymai buvo teisingi, o pirmas buvo neteisingas.')
+//    } else if (userAnswer1 === 6 && userAnswer2 !== 7 && userAnswer3 !== 8) {
+//       console.log('Nepatekai i kita lygi: pirmas atsakymas buvo teisingas, o kiti buvo neteisingi.')
+//    } else if (userAnswer1 !== 6 && userAnswer2 === 7 && userAnswer3 !== 8) {
+//       console.log('Nepatekai i kita lygi: antras atsakymas buvo teisingas, o kiti buvo neteisingi.')
+//    } else if (userAnswer1 !== 6 && userAnswer2 !== 7 && userAnswer3 === 8) {
+//       console.log('Nepatekai i kita lygi: trecias atsakymas buvo teisingas, o kiti buvo neteisingi.')
+//    } else {
+//       console.log('Nepatekai i kita lygi: visi atsakymai buvo neteisingi.')
+//    }
+// } else {
+//    console.log('Klaida. Nepavyko patekti i kita lygi.')
+// }
 
 // PASISVEIKINIMAS:
-let isLoggedIn = true
-let userName = 'Aldas'
-let time = new Date().getHours()
-console.log(time)
-let isBirthday = true
+// let isLoggedIn = true
+// let userName = 'Aldas'
+// let time = new Date().getHours()
+// console.log(time)
+// let isBirthday = true
 
-if (isLoggedIn === true) {
-   console.log(`Sveiki atvykę,! ${userName}`)
-   if (time >= 5 && time <= 12) {
-      console.log(`Labas rytas, šiuo metu yra ${time} h.`)
-   } else if (time >= 13 && time <= 18) {
-      console.log(`Labas diena, šiuo metu yra ${time} h.`)
-   } else {
-      console.log(`Labas vakaras, šiuo metu yra ${time} h.`)
-   }
-} else {
-   console.log(`Sveiki sveiki sveiki`)
-}
-if (isLoggedIn === true && isBirthday === true) {
-   console.log(`${userName} Sveikiname su gimtadieniu!`)
-}
+// if (isLoggedIn === true) {
+//    console.log(`Sveiki atvykę,! ${userName}`)
+//    if (time >= 5 && time <= 12) {
+//       console.log(`Labas rytas, šiuo metu yra ${time} h.`)
+//    } else if (time >= 13 && time <= 18) {
+//       console.log(`Labas diena, šiuo metu yra ${time} h.`)
+//    } else {
+//       console.log(`Labas vakaras, šiuo metu yra ${time} h.`)
+//    }
+// } else {
+//    console.log(`Sveiki sveiki sveiki`)
+// }
+// if (isLoggedIn === true && isBirthday === true) {
+//    console.log(`${userName} Sveikiname su gimtadieniu!`)
+// }
