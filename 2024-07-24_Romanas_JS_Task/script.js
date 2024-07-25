ba8a0ff025de5e076b97632de01ff1dba1b7b15e
brutall100 <37956008+brutall100@users.noreply.github.com>
2024-07-25 20:29:02 +0300

    Done 7th task with copilot help

--- 2024-07-24_Romanas_JS_Task/script.js
+++ 2024-07-24_Romanas_JS_Task/script.js
@@ -913,3 +913,325 @@ mainArray.map((element, index) => {
 })
 
 
+
+
+// 7 Task
+const posts = [
+   {
+     id: 1,
+     title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+     body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+   },
+   {
+     id: 2,
+     title: "qui est esse",
+     body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+   },
+   {
+     id: 3,
+     title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+     body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+   },
+   {
+     id: 4,
+     title: "eum et est occaecati",
+     body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+   },
+   {
+     id: 5,
+     title: "nesciunt quas odio",
+     body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+   },
+   {
+     id: 6,
+     title: "dolorem eum magni eos aperiam quia",
+     body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+   },
+   {
+     id: 7,
+     title: "magnam facilis autem",
+     body: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+   },
+   {
+     id: 8,
+     title: "dolorem dolore est ipsam",
+     body: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+   },
+   {
+     id: 9,
+     title: "nesciunt iure omnis dolorem tempora et accusantium",
+     body: "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
+   },
+   {
+     id: 10,
+     title: "optio molestias id quia eum",
+     body: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+   },
+   {
+     id: 11,
+     title: "et ea vero quia laudantium autem",
+     body: "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
+   },
+   {
+     id: 12,
+     title: "in quibusdam tempore odit est dolorem",
+     body: "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+   },
+   {
+     id: 13,
+     title: "dolorum ut in voluptas mollitia et saepe quo animi",
+     body: "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
+   },
+   {
+     id: 14,
+     title: "voluptatem eligendi optio",
+     body: "fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum"
+   },
+   {
+     id: 15,
+     title: "eveniet quod temporibus",
+     body: "reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae"
+   },
+   {
+     id: 16,
+     title: "sint suscipit perspiciatis velit dolorum rerum ipsa laboriosam odio",
+     body: "suscipit nam nisi quo aperiam aut\nasperiores eos fugit maiores voluptatibus quia\nvoluptatem quis ullam qui in alias quia est\nconsequatur magni mollitia accusamus ea nisi voluptate dicta"
+   },
+   {
+     id: 17,
+     title: "fugit voluptas sed molestias voluptatem provident",
+     body: "eos voluptas et aut odit natus earum\naspernatur fuga molestiae ullam\ndeserunt ratione qui eos\nqui nihil ratione nemo velit ut aut id quo"
+   },
+   {
+     id: 18,
+     title: "voluptate et itaque vero tempora molestiae",
+     body: "eveniet quo quis\nlaborum totam consequatur non dolor\nut et est repudiandae\nest voluptatem vel debitis et magnam"
+   },
+   {
+     id: 19,
+     title: "adipisci placeat illum aut reiciendis qui",
+     body: "illum quis cupiditate provident sit magnam\nea sed aut omnis\nveniam maiores ullam consequatur atque\nadipisci quo iste expedita sit quos voluptas"
+   },
+   {
+     id: 20,
+     title: "doloribus ad provident suscipit at",
+     body: "qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo"
+   },
+   {
+     id: 21,
+     title: "asperiores ea ipsam voluptatibus modi minima quia sint",
+     body: "repellat aliquid praesentium dolorem quo\nsed totam minus non itaque\nnihil labore molestiae sunt dolor eveniet hic recusandae veniam\ntempora et tenetur expedita sunt"
+   },
+   {
+     id: 22,
+     title: "dolor sint quo a velit explicabo quia nam",
+     body: "eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+   },
+   {
+     id: 23,
+     title: "maxime id vitae nihil numquam",
+     body: "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis"
+   },
+   {
+     id: 24,
+     title: "autem hic labore sunt dolores incidunt",
+     body: "enim et ex nulla\nomnis voluptas quia qui\nvoluptatem consequatur numquam aliquam sunt\ntotam recusandae id dignissimos aut sed asperiores deserunt"
+   },
+   {
+     id: 25,
+     title: "rem alias distinctio quo quis",
+     body: "ullam consequatur ut\nomnis quis sit vel consequuntur\nipsa eligendi ipsum molestiae et omnis error nostrum\nmolestiae illo tempore quia et distinctio"
+   }
+ ]
+
+ //  7. Naudojant posts masyvą, atlikti žemiau nurodytas užduotis ir atsakymus išvesti į konsolę. Visoms užduotis kurti funkciją ir jai, kaip parametrą, paduoti nurodytą masyvą.
+// 7.1. Gauti pirmo masyvo nario reikšmę.
+function getFirstElement(array) {
+   return array[0]
+}
+console.log(getFirstElement(posts))
+
+// 7.2. Gauti paskutinio masyvo nario reikšmę.
+function getLastElement(array) {
+   return array[array.length - 1]
+}
+console.log(getLastElement(posts))
+
+// 7.3. Gauti 15 masyvo nario reikšmę.
+function get15thElement(array) {
+   return array[14]
+}
+console.log(get15thElement(posts))
+
+// 7.4. Gauti 13 masyvo nario reikšmę skaičiuojant nuo galo.
+function get13thElementFromEnd(array) {
+   return array[array.length - 13]
+}
+console.log(get13thElementFromEnd(posts))
+
+// 7.5. Gauti vidurinio masyvo nario reikšmę.
+function getMiddleElement(array) {
+   return array[Math.floor(array.length / 2)]
+}
+console.log(getMiddleElement(posts))
+
+// 7.6. Gauti pirmų keturių masyvo narių reikšmes.
+function getFirstFourElements(array) {
+   return array.slice(0, 4)
+}
+console.log(getFirstFourElements(posts))
+
+// 7.7. Gauti paskutinių penkių masyvo narių reikšmes.
+function getLastFiveElements(array) {
+   return array.slice(-5)
+}
+console.log(getLastFiveElements(posts))
+
+// 7.8. Gauti nuo penkto iki penkiolikto masyvo narių reikšmes.
+function getFrom5to15Elements(array) {
+   return array.slice(4, 15)
+}
+console.log(getFrom5to15Elements(posts))
+
+// 7.9. Gauti kas antro masyvo nario reikšmes.
+function getEverySecondElement(array) {
+   return array.filter((element, index) => index % 2 !== 0)
+}
+console.log(getEverySecondElement(posts))
+
+// 7.10. Gauti pirmo ir paskutinio masyvo narių reikšmes.
+function getFirstAndLastElement(array) {
+   return [array[0], array[array.length - 1]]
+}
+console.log(getFirstAndLastElement(posts))
+
+// 7.11. Gauti pirmą masyvo narį ir į konsolę išvesti jo property "title" reikšmę.
+function getFirstElementTitle(array) {
+   return array[0].title
+}
+console.log(getFirstElementTitle(posts))  
+
+// 7.12. Gauti antrą masyvo narį ir į konsolę išvesti jo property "body" reikšmę.
+function getSecondElementBody(array) {
+   return array[1].body
+}
+console.log(getSecondElementBody(posts))
+
+// 7.13. Gauti trečią masyvo narį ir į konsolę išvesti jo property "title" ir "body" reikšmes tokiu formatu: Post title is: 'post title' and the content is: 'post content'.
+function getThirdElementTitleAndBody(array) {
+   return `Post title is: '${array[2].title}' and the content is: '${array[2].body}'`
+}
+console.log(getThirdElementTitleAndBody(posts))
+
+// 7.14. Išvesti visus masyvo narius į konsolę.
+function getAllElements(array) {
+   array.forEach((element) => console.log(element))
+}
+getAllElements(posts)
+
+// 7.15. Išvesti visų masyvo narių property "title" į konsolę.
+function getAllElementsTitle(array) {
+   array.forEach((element) => console.log(element.title))
+}
+getAllElementsTitle(posts)
+
+// 7.16. Išvesti visų masyvo narių property "body" į konsolę.
+function getAllElementsBody(array) {
+   array.forEach((element) => console.log(element.body))
+}
+getAllElementsBody(posts)
+
+// 7.17. Išvesti visų masyvo narių property "title" ir "body" į konsolę tokiu formatu: Title: "post title". Content: "post content".
+function getAllElementsTitleAndBody(array) {
+   array.forEach((element) => console.log(`Title: "${element.title}". Content: "${element.body}"`))
+}
+getAllElementsTitleAndBody(posts)
+
+// 7.18. Išvesti pirmų keturių masyvo narių property "title" ir "body" į konsolę tokiu formatu: Title: "post title". Content: "post content".
+function getFirstFourElementsTitleAndBody(array) {
+   array.slice(0, 4).forEach((element) => console.log(`Title: "${element.title}". Content: "${element.body}"`))
+}
+getFirstFourElementsTitleAndBody(posts)
+
+// 7.19. Išvesti paskutinių septynių masyvo narių property "title" ir "body" į konsolę tokiu formatu: Title: "post title". Content: "post content".
+function getLastSevenElementsTitleAndBody(array) {
+   array.slice(-7).forEach((element) => console.log(`Title: "${element.title}". Content: "${element.body}"`))
+}
+getLastSevenElementsTitleAndBody(posts)
+
+// 7.20. Sukurti naują masyvą, kurio nariai bus posts masyvo narių property "title" reikšmė, pvz.: ['Pirmo posts masyvo nario title', 'Antro posts masyvo nario title', 'Trečio posts masyvo nario title'...]
+function createTitlesArray(arr) {
+   const titles = arr.map(post => post.title);
+   console.log(titles);
+}
+createTitlesArray(posts);
+
+// 7.21. Sukurti naują masyvą, kurio nariai bus posts masyvo narių property "title" ir "body" tokiu formatu: Title: "post title". Content: "post content".
+function createTitleAndBodyArray(arr) {
+   const titlesAndBodies = arr.map(post => `Title: "${post.title}". Content: "${post.body}"`);
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArray(posts);
+
+// 7.22. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių pavadinimas prasideda raide "s".
+function createTitleAndBodyArrayStartingWithS(arr) {
+   const titlesAndBodies = arr.filter(post => post.title[0].toLowerCase() === 's').map(post => `Title: "${post.title}". Content: "${post.body}"`);
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayStartingWithS(posts);
+
+// 7.23. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių pavadinimas baigiasi "t" arba "m".
+function createTitleAndBodyArrayEndingWithTOrM(arr) {
+   const titlesAndBodies = arr.filter(post => post.title.slice(-1).toLowerCase() === 't' || post.title.slice(-1).toLowerCase() === 'm').map(post => `Title: "${post.title}". Content: "${post.body}"`);
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayEndingWithTOrM(posts);
+
+// 7.24. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių pavadinimas turi daugiau simbolių nei 15.
+function createTitleAndBodyArrayWithMoreThan15Characters(arr) {
+   const titlesAndBodies = arr.filter(post => post.title.length > 15).map(post => `Title: "${post.title}". Content: "${post.body}"`);  
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayWithMoreThan15Characters(posts);
+
+// 7.25. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių pavadinimas turi mažiau simbolių nei 20 ir kurių "body" turi daugiau simbolių nei 50.
+function createTitleAndBodyArrayWithLessThan20CharactersAndMoreThan50BodyCharacters(arr) {
+   const titlesAndBodies = arr.filter(post => post.title.length < 20 && post.body.length > 50).map(post => `Title: "${post.title}". Content: "${post.body}"`);
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayWithLessThan20CharactersAndMoreThan50BodyCharacters(posts);
+
+// 7.26. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių pavadinimas turi tarp 20 ir 30 simbolių ir kurių "body" turi tarp 70 ir 130 simbolių.
+function createTitleAndBodyArrayWithBetween20And30CharactersAndBetween70And130BodyCharacters(arr) {
+   const titlesAndBodies = arr.filter(post => post.title.length > 20 && post.title.length < 30 && post.body.length > 70 && post.body.length < 130).map(post => `Title: "${post.title}". Content: "${post.body}"`);
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayWithBetween20And30CharactersAndBetween70And130BodyCharacters(posts);
+
+// 7.27. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių pavadinimas turi simbolių junginį "it", o "body" turi simbolių junginį "quo".
+function createTitleAndBodyArrayWithItInTitleAndQuoInBody(arr) {
+   const titlesAndBodies = arr.filter(post => post.title.toLowerCase().includes('it') && post.body.toLowerCase().includes('quo')).map(post => `Title: "${post.title}". Content: "${post.body}"`);
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayWithItInTitleAndQuoInBody(posts);
+
+// 7.28. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių "body" turi žodį "sit". Jeigu žodis turi šį simbolių junginį, tačiau tai nėra pilna jo reikšmė (pvz. žodžiai "sitto" ar "quasit"), jie neturėtų būti atvaizduojami.
+function createTitleAndBodyArrayWithSitInBody(arr) {
+   const titlesAndBodies = arr.filter(post => post.body.toLowerCase().includes('sit') && !post.body.toLowerCase().includes('sitto') && !post.body.toLowerCase().includes('quasit')).map(post => `Title: "${post.title}". Content: "${post.body}"`);
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayWithSitInBody(posts);
+
+// 7.29. Tokiu pačiu formatu, kaip ankstesnėje užduotyje, sukurti naują masyvą tik iš tų posts masyvo narių kurių pavadinimas turi daugiau nei 3 "a" raides, o "body" turi mažiau nei 7 "o" raides.
+function createTitleAndBodyArrayWithMoreThan3AInTitleAndLessThan7OInBody(arr) {
+   const titlesAndBodies = arr.filter(post => post.title.toLowerCase().split('a').length - 1 > 3 && post.body.toLowerCase().split('o').length - 1 < 7).map(post => `Title: "${post.title}". Content: "${post.body}"`);  
+   console.log(titlesAndBodies);
+}
+createTitleAndBodyArrayWithMoreThan3AInTitleAndLessThan7OInBody(posts);
+
+// 7.30. Sukurti naują masyvą, kurio nariai bus posts masyvo narių property "title" reikšmė, tačiau surūšiuoti nuo mažiausiai iki daugiausiai raidžių "body" property turinčių narių. Papildomai, prie išvesto teksto pridėti ir "body" raidžių skaičių.
+function createTitlesArraySortedByBodyLength(arr) {
+   const titles = arr.map(post => ({ title: post.title, bodyLength: post.body.length })).sort((a, b) => a.bodyLength - b.bodyLength).map(post => `${post.title}. Body length: ${post.bodyLength}`);
+   console.log(titles);
+}
+createTitlesArraySortedByBodyLength(posts);
