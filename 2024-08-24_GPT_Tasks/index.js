c715af132979fdb95ce055c2fd49c30840844af4
Aldas <brutall100@gmail.com>
2024-08-24 17:49:02 +0300

    Just some div

--- 2024-08-24_GPT_Tasks/index.js
+++ 2024-08-24_GPT_Tasks/index.js
@@ -75,10 +75,51 @@ init();
 
 function createUserContent(data) {
     console.log(data);
+    const user = data[0];
     // Add some code here to create and return user content.
+    
+    // Create a div element
+    const userElement = document.createElement('div');
+    userElement.classList.add('user');
+
+    // Create a h2 element
+    const nameElement = document.createElement('h2');
+    nameElement.textContent = user.name;
+
+    // Create a p element
+    const emailElement = document.createElement('p');
+    emailElement.textContent = user.email;
+
+    // Append elements to the user element
+    userElement.append(nameElement, emailElement);
+
+    return userElement;
 }
 
 function createUserPosts(posts) {
     console.log(posts);
-    // Add some code here to create and return posts.
+
+    // Create a div element
+    const postsElement = document.createElement('div');
+    postsElement.classList.add('posts');
+
+    // Create a h2 element
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = 'Posts';
+
+    // Create a ul element
+    const listElement = document.createElement('ul');
+
+    // Create li elements
+    posts.forEach(post => {
+        const postElement = document.createElement('li');
+        postElement.textContent = post.title;
+        listElement.append(postElement);
+    });
+
+    // Append elements to the posts element
+    postsElement.append(titleElement, listElement);
+
+    return postsElement;
+
 }
