9683bd6a29097372b0d6d4cfa18e3185663d02ba
Aldas <brutall100@gmail.com>
2024-09-02 20:22:42 +0300

    Code done

--- 2024-08-27_Fetch_Api_PrcticeNT/add.js
+++ 2024-08-27_Fetch_Api_PrcticeNT/add.js
@@ -1,12 +1,35 @@
-document.querySelector('form').addEventListener('submit', function (event) {
+document.querySelector('form').addEventListener('submit', async function (event) {
     event.preventDefault();
+
     const imageUrl = document.querySelector('#img').value;
     const price = document.querySelector('#price').value;
     const description = document.querySelector('#description').value;
     const city = document.querySelector('#city').value;
+
     const newProperty = { city, price, description, imageUrl };
     console.log(newProperty);
-    // data.push(newProperty);
-    // processData(data);
-    event.target.reset(event);
+
+    try {
+        const response = await fetch('https://robust-safe-crafter.glitch.me/', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify(newProperty), 
+        });
+
+        if (response.ok) {
+            console.log('Data successfully sent to the server.');
+            fetchData(); 
+        } else {
+            console.error('Failed to send data to the server.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+    event.target.reset();
 });
+
+
+
